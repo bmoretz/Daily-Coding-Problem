adc552cf0c7735bdac41807f3df5cb5b676fae5a
Brandon Moretz <bmoretz@ionicsolutions.net>
2020-01-13 12:00:33 -0500

    dcp 3.2
    add linked list numbers

--- problems/linkedlist/__init__.py
+++ problems/linkedlist/__init__.py
@@ -46,7 +46,7 @@ class SLinkedList:
         result = []
         c = self.head
 
-        while( c.get_next() != None ):
+        while( c != None ):
             result.append(c.data)
             c = c.get_next()
         
@@ -81,3 +81,44 @@ class SLinkedList:
             c = c.get_next()
 
         return result
+
+'''3.2
+We can represent an integer in a linked list format by having each node represent a digit in the number. The nodes are connected in reverse order, such that the number
+
+54321
+
+is represented by the following linked list:
+
+1 -> 2 -> 3 -> 4 -> 5
+
+Given two linked lists in this format, return their sum.
+
+for example,
+
+9 -> 9
+5 -> 2
+
+Should return 124 (4 -> 2 -> 1)
+'''
+def add_numbers(list1, list2):
+    result = SLinkedList()
+    
+    # storage for current elements and remainder
+    e1, e2, rem = list1.head, list2.head, 0
+
+    while e1 != None or e2 != None or rem != 0:
+
+        v1 = 0 if e1 is None else e1.data
+        v2 = 0 if e2 is None else e2.data
+
+        current = v1 + v2 + rem
+
+        value = current % 10
+        rem = 0 if e1 == None or e2 == None else int( current / 10)
+
+        result.push_back( value )
+        
+        e1 = None if e1 is None else e1.get_next()
+        e2 = None if e2 is None else e2.get_next()
+
+    return result
--- tests/linkedlist_test.py
+++ tests/linkedlist_test.py
@@ -1,8 +1,9 @@
 import unittest
 
 from problems.linkedlist import Node, SLinkedList
+from problems.linkedlist import add_numbers
 
-class Test_SingleLinkedList(unittest.TestCase):
+class Test_SingleLinkedListReverse(unittest.TestCase):
     
     def setUp(self):
         pass
@@ -16,4 +17,25 @@ class Test_SingleLinkedList(unittest.TestCase):
         
         sl.reverse()
 
-        assert sl.elements() == [9, 8, 7, 6, 5, 4, 3, 2, 1]
+        assert sl.elements() == [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+
+class Test_SingleLinkedListAdd(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+
+    def test_case1(self):
+
+        l1, l2 = SLinkedList(), SLinkedList()
+
+        l1.push_back(9)
+        l1.push_back(9)
+
+        l2.push_back(5)
+        l2.push_back(2)
+
+        result = add_numbers(l1, l2)
+        
+        print(result.elements())
+
+        assert result.elements() == [4, 2, 1]
