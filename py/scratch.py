fa8e3336cb71e92f9814667713471a0974ddaf59
Brandon Moretz <bmoretz82@gmail.com>
2020-06-02 10:41:27 -0400

    python sum swap
    python sum swap

--- py/scratch.py
+++ py/scratch.py
@@ -1,111 +1,36 @@
-'''T9.
+'''Sum Swap.
 
-On old cell phones, users typed on a numeric pad and the phone
-would provide a list of words that matched these numbers. Each
-digit mapped to a set of 0-4 letters. Implement an algorithm
-to return a list of matching words, given a sequence of digits.
-You are provide a list of valid words (provided in whatever data
-structure you'd like). The mapping is shown in the diagram below.
-
-    1       2       3
-           abc     def
-    4       5       6
-   ghi     jkl     mno
-   7        8       9
-  pqrs     tvu    wxyz
-            0
+Given two arrays of integers, find a pair of values (one value from
+each array) that you can swap to give the two arrays the same
+sum.
 
 EXAMPLE
-
-Input: 8733
-Output: tree, used
+Input: {4, 1, 2, 1, 1, 2} and {3, 6, 3, 3}
+Output: {1, 3}
 '''
 
-from collections import defaultdict
+def sum_swap(arr1, arr2):
 
-class T9():
-
-    class Dictionary():
-        
-        class Node():
-            def __init__(self, value, parent=None):
-                self.value = value
-                self.parent = parent
-                self.children = defaultdict(list)
-
-        def __init__(self):
-            self.root = self.Node(None)
-
-        def insert_word(self, word):
-            
-            node = self.root
-
-            for letter in [ w for w in word] + ['*']:
-                if not letter in node.children:
-                    node.children[letter] = self.Node(letter, node)
-
-                node = node.children[letter]
-        
-        def get_matches(self, letters, nodes=None):
-
-            nodes = [self.root] if not nodes else nodes
-            results = []
-
-            for letter in letters:
-                for node in nodes:
-                    for child in node.children:
-                        if child == letter:
-                            results += [node.children[letter]]
-
-            return results
-
-    def __init__(self, word_list):
-        pad = defaultdict(list)
-
-        pad[2] = ['a', 'b', 'c']
-        pad[3] = ['d', 'e', 'f']
-        pad[4] = ['g', 'h', 'i']
-        pad[5] = ['j', 'k', 'l']
-        pad[6] = ['m', 'n', 'o']
-        pad[7] = ['p', 'q', 'r', 's']
-        pad[8] = ['t', 'v', 'u']
-        pad[9] = ['w', 'x', 'y', 'z']
     
-        self.pad = pad
+    sumA, sumB = sum(arr1), sum(arr2)
 
-        dictionary = self.Dictionary()
+    delta = (sumB - sumA)/2
 
-        for word in word_list:
-            dictionary.insert_word(word)
+    ys = set(arr2)
 
-        self.dictionary = dictionary
+    results = set()
 
-    def get_matches(self, num):
-        
-        if not num: return None
+    for x in arr1:
 
-        word_matches = []
-        nodes = []
+        t = x + delta
 
-        for digit in str(num):
-            letters = self.pad[ int(digit) ]
-            nodes = self.dictionary.get_matches(letters, nodes)
+        if t in ys:
+            results.add(( int(x), int(t)))
 
-        for node in nodes:
-            word = ''
+    return results
 
-            while node.parent:
-                word = node.value + word
-                node = node.parent
-            
-            word_matches.append(word)
+arr1, arr2 = [4, 1, 2, 1, 1, 2, 4, 2, 1, 2], [1, 6, 2, 3, 5, 1]
 
-        return word_matches
-
-words = ['used', 'tree', 'angry', 'world', 'alpha', 'trip', 'usage', 'triangle', 'apple', 'milk']
-
-calc = T9(word_list=words)
-
-results = calc.get_matches(26479)
+results = sum_swap(arr1, arr2)
 
 print(results)
--- py/tests/math_test.py
+++ py/tests/math_test.py
@@ -328,3 +328,63 @@ class Test_ContiguousSeq1(unittest.TestCase):
         expected = 8
 
         assert actual == expected
+
+from py.problems.math import sum_swap
+class Test_SumSwap1(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+
+    def test_none(self):
+
+        arr = None
+
+        actual = sum_swap(arr, arr)
+        expected = None
+
+        assert actual == expected
+
+    def test_case1(self):
+
+        arr1, arr2 = [4, 1, 2, 1, 1, 2], [3, 6, 3, 3]
+
+        actual = sum_swap(arr1, arr2)
+        expected = {(4, 6), (1, 3)}
+
+        assert actual == expected
+
+    def test_case2(self):
+
+        arr1, arr2 = [4, 1, 2, 1, 1, 2], [3, 6, 3, 3, 2]
+
+        actual = sum_swap(arr1, arr2)
+        expected = set()
+
+        assert actual == expected
+
+    def test_case3(self):
+
+        arr1, arr2 = [4, 1, 2, 1, 1, 2], [3, 6, 3, 3, 5, 1]
+
+        actual = sum_swap(arr1, arr2)
+        expected = {(1, 6)}
+
+        assert actual == expected
+
+    def test_case4(self):
+
+        arr1, arr2 = [4, 1, 2, 1, 1, 2, 4, 2, 1], [1, 6, 2, 3, 5, 1]
+
+        actual = sum_swap(arr1, arr2)
+        expected = {(1, 1), (2, 2)}
+
+        assert actual == expected
+
+    def test_case5(self):
+
+        arr1, arr2 = [4, 1, 2, 1, 1, 2, 4, 2, 1, 2], [1, 6, 2, 3, 5, 1]
+
+        actual = sum_swap(arr1, arr2)
+        expected = {(2, 1), (4, 3)}
+
+        assert actual == expected
