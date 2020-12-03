fddc355c03859aff2faa688b464843ac175cd8c3
Brandon Moretz <bmoretz82@gmail.com>
2020-12-03 15:48:38 -0500

    Python leetcode #138, deep copy random
    Python leetcode #138, deep copy random

--- py/leetcode/linkedlist/__init__.py
+++ py/leetcode/linkedlist/__init__.py
@@ -899,7 +899,7 @@ class circular_insert:
         node.next = sentinel.next
 
         return sentinel.next
-        
+
     def to_list(self, head) -> List[int]:
 
         node, result = head, []
@@ -945,3 +945,98 @@ class circular_insert:
         prev.next = self.ListNode(insertVal, cur)
         
         return head
+
+'''
+138. Copy List with Random Pointer.
+
+A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+
+Return a deep copy of the list.
+
+The Linked List is represented in the input/output as a list of n nodes. Each node is represented as a pair of [val, random_index] where:
+
+val: an integer representing Node.val
+random_index: the index of the node (range from 0 to n-1) where random pointer points to, or null if it does not point to any node.
+ 
+
+Example 1:
+
+
+Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
+Example 2:
+
+Input: head = [[1,1],[2,1]]
+Output: [[1,1],[2,1]]
+Example 3:
+
+Input: head = [[3,null],[3,0],[3,null]]
+Output: [[3,null],[3,0],[3,null]]
+Example 4:
+
+Input: head = []
+Output: []
+Explanation: Given linked list is empty (null pointer), so return null.
+
+Constraints:
+
+-10000 <= Node.val <= 10000
+Node.random is null or pointing to a node in the linked list.
+The number of nodes will not exceed 1000.
+'''
+
+class deep_copy_random:
+
+    from typing import List
+    class ListNode:
+
+        def __init__(self, val, nxt : 'ListNode' = None, random : 'ListNode' = None) -> 'ListNode':
+            self.val = val
+            self.next = nxt
+            self.random = random
+
+    def build_random_list(self, values : List[List[int]]) -> ListNode:
+
+        sentinel = self.ListNode(0)
+        node, nodes = sentinel, {}
+
+        for index, val in enumerate(values):
+            node.next = self.ListNode(val[0])
+            node = node.next
+
+            nodes[index] = node
+
+        for index, val in enumerate(values):
+            
+            rnd_ptr = val[1]
+
+            if rnd_ptr:
+                nodes[index].random = nodes[rnd_ptr]
+
+        return sentinel.next
+
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        
+        sentinel = self.ListNode
+        new_node = sentinel
+        
+        node, seen = head, {}
+        
+        while node:
+            
+            new_node.next = self.ListNode(node.val)
+            
+            seen[node] = new_node.next
+            node, new_node = node.next, new_node.next
+            
+            
+        node = head
+        
+        for index in range(len(seen)):
+            
+            if node.random:
+                seen[node].random = seen[node.random]
+            
+            node = node.next
+            
+        return sentinel.next
--- py/scratch.py
+++ py/scratch.py
@@ -1,115 +1,112 @@
 
 '''
-708. Insert into a Sorted Circular Linked List.
+138. Copy List with Random Pointer.
 
-Given a node from a Circular Linked List which is sorted in ascending order, write a function to insert a 
-value insertVal into the list such that it remains a sorted circular list. The given node can be a reference to 
-any single node in the list, and may not be necessarily the smallest value in the circular list.
+A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
 
-If there are multiple suitable places for insertion, you may choose any place to insert the new 
-value. After the insertion, the circular list should remain sorted.
+Return a deep copy of the list.
 
-If the list is empty (i.e., given node is null), you should create a new single circular list and return the reference to 
-that single node. Otherwise, you should return the original given node.
+The Linked List is represented in the input/output as a list of n nodes. Each node is represented as a pair of [val, random_index] where:
+
+val: an integer representing Node.val
+random_index: the index of the node (range from 0 to n-1) where random pointer points to, or null if it does not point to any node.
+ 
 
 Example 1:
 
-Input: head = [3,4,1], insertVal = 2
-Output: [3,4,1,2]
 
-Explanation: In the figure above, there is a sorted circular list of three elements. You are given a reference to 
-the node with value 3, and we need to insert 2 into the list. The new node should be inserted between node 1 and node 3. After the insertion, the list should look like this, and we should still return node 3.
-
+Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
 Example 2:
 
-Input: head = [], insertVal = 1
-Output: [1]
-Explanation: The list is empty (given head is null). We create a new single circular list and return the reference to that single node.
+Input: head = [[1,1],[2,1]]
+Output: [[1,1],[2,1]]
 Example 3:
 
-Input: head = [1], insertVal = 0
-Output: [1,0]
+Input: head = [[3,null],[3,0],[3,null]]
+Output: [[3,null],[3,0],[3,null]]
+Example 4:
+
+Input: head = []
+Output: []
+Explanation: Given linked list is empty (null pointer), so return null.
 
 Constraints:
 
-0 <= Number of Nodes <= 5 * 10^4
--10^6 <= Node.val <= 10^6
--10^6 <= insertVal <= 10^6
+-10000 <= Node.val <= 10000
+Node.random is null or pointing to a node in the linked list.
+The number of nodes will not exceed 1000.
 '''
 
-class circular_insert:
+class deep_copy_random:
 
     from typing import List
-
     class ListNode:
-        def __init__(self, val=None, next=None):
+
+        def __init__(self, val, nxt : 'ListNode' = None, random : 'ListNode' = None) -> 'ListNode':
             self.val = val
-            self.next = next
-    
-    def make_list(self, values : List[int] ) -> ListNode:
+            self.next = nxt
+            self.random = random
 
-        sentinel = self.ListNode()
-        node = sentinel
+    def build_random_list(self, values : List[List[int]]) -> ListNode:
 
-        for val in values:
-            node.next = self.ListNode(val)
+        sentinel = self.ListNode(0)
+        node, nodes = sentinel, {}
+
+        for index, val in enumerate(values):
+            node.next = self.ListNode(val[0])
             node = node.next
-        
-        node.next = sentinel.next
+
+            nodes[index] = node
+
+        for index, val in enumerate(values):
+            
+            rnd_ptr = val[1]
+
+            if rnd_ptr:
+                nodes[index].random = nodes[rnd_ptr]
 
         return sentinel.next
 
-    def to_list(self, head) -> List[int]:
-
-        node, result = head, []
-
-        while True:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        
+        sentinel = self.ListNode
+        new_node = sentinel
+        
+        node, seen = head, {}
+        
+        while node:
             
-            result.append(node.val)
+            new_node.next = self.ListNode(node.val)
+            
+            seen[node] = new_node.next
+            node, new_node = node.next, new_node.next
+            
+            
+        node = head
+        
+        for index in range(len(seen)):
+            
+            if node.random:
+                seen[node].random = seen[node.random]
+            
             node = node.next
-
-            if node == head:
-                break
-
-        return result
-
-    def insert(self, head: ListNode, insertVal: int) -> ListNode:
-        
-        new_node = self.ListNode( insertVal )
-        new_node.next = new_node
-        
-        if not head:
-            return new_node
-        
-        prev, cur = head, head.next
-        to_insert = False
-        
-        while True:
             
-            if prev.val <= insertVal <= cur.val:
-                to_insert = True
-            elif prev.val > cur.val:
-                if insertVal >= prev.val or insertVal <= cur.val:
-                    to_insert = True
-                    
-            if to_insert:
-                prev.next = self.ListNode(insertVal, cur)
-                return head
-            
-            prev, cur = cur, cur.next
-            
-            if prev == head:
-                break
-        
-        prev.next = self.ListNode(insertVal, cur)
-        
-        return head
+        return sentinel.next
 
 
-head = circular_insert().make_list([3,4,1])
+list_str = [[7,None],[13,0],[11,4],[10,2],[1,0]]
+head = deep_copy_random().build_random_list(list_str)
 
-new_list = circular_insert().insert(head, 2)
+new_head = deep_copy_random().copyRandomList(head)
 
-actual = circular_insert().to_list(new_list)
+old, new = head, new_head
 
-expected = [3,4,1,2]
+while old or new:
+
+    assert old.val == new.val
+
+    if old.random or new.random:
+        assert old.random.val == new.random.val
+
+    old, new = old.next, new.next
