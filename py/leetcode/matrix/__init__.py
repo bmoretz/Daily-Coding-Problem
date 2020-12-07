6db22ef4f568330e5ee47a13a8d5cd8798d9a9f0
Brandon Moretz <bmoretz82@gmail.com>
2020-12-07 15:25:01 -0500

    Python leetcode #498, diagonal traverse matrix
    Python leetcode #498, diagonal traverse matrix

--- py/leetcode/matrix/__init__.py
+++ py/leetcode/matrix/__init__.py
@@ -57,13 +57,3 @@ class diagonal_traverse():
             index+=1
             
         return result
-
-mat = [
- [ 1, 2, 3 ],
- [ 4, 5, 6 ],
- [ 7, 8, 9 ]
-]
-
-actual = diagonal_traverse.findDiagonalOrder(mat)
-
-expected = [1,2,4,7,5,3,6,8,9]
