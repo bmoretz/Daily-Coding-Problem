8fec4a11f9163753431b5a88405c8e3ab525c867
Brandon Moretz <bmoretz82@gmail.com>
2020-12-07 14:04:21 -0500

    Python leetcode #747, dominant index
    Python leetcode #747, dominant index

--- py/scratch.py
+++ py/scratch.py
@@ -1,58 +1,53 @@
 
 '''
-724. Find Pivot Index.
+747. Largest Number At Least Twice of Others.
 
-Given an array of integers nums, write a method that returns the "pivot" index of this array.
+In a given integer array nums, there is always exactly one largest element.
 
-We define the pivot index as the index where the sum of all the numbers to the left of the 
-index is equal to the sum of all the numbers to the right of the index.
+Find whether the largest element in the array is at least twice as much as every other number in the array.
 
-If no such index exists, we should return -1. If there are multiple pivot indexes, you should return the left-most pivot index. 
+If it is, return the index of the largest element, otherwise return -1.
 
 Example 1:
 
-Input: nums = [1,7,3,6,5,6]
-Output: 3
-
-Explanation:
-
-The sum of the numbers to the left of index 3 (nums[3] = 6) is equal to the sum of numbers to the right of index 3.
-Also, 3 is the first index where this occurs.
+Input: nums = [3, 6, 1, 0]
+Output: 1
+Explanation: 6 is the largest integer, and for every other number in the array x,
+6 is more than twice as big as x.  The index of value 6 is 1, so we return 1.
+ 
 
 Example 2:
 
-Input: nums = [1,2,3]
+Input: nums = [1, 2, 3, 4]
 Output: -1
-Explanation:
-There is no index that satisfies the conditions in the problem statement.
+Explanation: 4 isn't at least as big as twice the value of 3, so we return -1.
 
-Constraints:
+Note:
 
-The length of nums will be in the range [0, 10000].
-Each element nums[i] will be an integer in the range [-1000, 1000].
+nums will have a length in the range [1, 50].
+Every nums[i] will be an integer in the range [0, 99].
 '''
 
-class find_pivot:
+class dominate_index:
 
     from typing import List
-
+    
     @staticmethod
-    def pivotIndex(nums: List[int]) -> int:
+    def dominantIndex(self, nums: List[int]) -> int:
         
-        left_sum, right_sum = 0, sum(nums)
+        max_index, max_num = float('-inf'), float('-inf')
         
-        for index in range(len(nums)):
+        for index, num in enumerate(nums):
             
-            right_sum -= nums[index]
+            if num > max_num:
+                max_index, max_num = index, num
+        
+        for num in nums:
             
-            if left_sum == right_sum:
-                return index
+            if num != max_num and num*2 > max_num:
+                return -1
             
-            left_sum += nums[index]
-            
-        return -1
-            
+        return max_index
 
-actual = find_pivot.pivotIndex([1,7,3,6,5,6])
-
-expected = 3
+actual = dominate_index.dominantIndex([3, 6, 1, 0])
+expected = 1
--- py/tests/leetcode/arr_test.py
+++ py/tests/leetcode/arr_test.py
@@ -115,3 +115,24 @@ class Test_FindPivot(unittest.TestCase):
         expected = 3
 
         assert actual == expected
+
+from py.leetcode.arr import dominate_index
+
+class Test_DominantIndex(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+
+    def test_case1(self):
+        
+        actual = dominate_index.dominantIndex([3, 6, 1, 0])
+        expected = 1
+
+        assert actual == expected
+
+    def test_case2(self):
+        
+        actual = dominate_index.dominantIndex([1, 2, 3, 4])
+        expected = -1
+
+        assert actual == expected        
