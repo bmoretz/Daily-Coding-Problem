e233154dfc7b23de55a10fa84598259fc85b2616
Brandon Moretz <bmoretz82@gmail.com>
2021-01-31 11:44:20 -0500

    C++ leetcode #136, single number
    C++ leetcode #136, single number

--- cpp/leetcode/hashtable.h
+++ cpp/leetcode/hashtable.h
@@ -416,4 +416,79 @@ namespace leetcode::hashtable
 		{'V', 5},
 		{'I', 1}
 	};
+
+	/*136. Single Number.
+
+	Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+	Follow up: Could you implement a solution with a linear runtime complexity and without using extra memory?
+
+
+	Example 1:
+
+	Input: nums = [2,2,1]
+	Output: 1
+	Example 2:
+
+	Input: nums = [4,1,2,1,2]
+	Output: 4
+	Example 3:
+
+	Input: nums = [1]
+	Output: 1
+
+
+	Constraints:
+
+	1 <= nums.length <= 3 * 104
+	-3 * 104 <= nums[i] <= 3 * 104
+	Each element in the array appears twice except for one element which appears only once.
+
+	*/
+
+	class single_number
+	{
+
+	public:
+
+		/// <summary>
+		/// O(N) run-time, O(1) space
+		/// </summary>
+		/// <param name="numbers">numbers</param>
+		/// <returns>the number with 1 occurence</returns>
+		static int singleNumber1( const std::vector<int>& numbers )
+		{
+			auto counts = std::unordered_map<int, int>();
+
+			for( auto num : numbers )
+			{
+				counts[ num ]++;
+			}
+
+			for( auto& [k, v] : counts )
+			{
+				if( v == 1 ) return k;
+			}
+
+			return -1;
+		}
+
+		/// <summary>
+		/// O(N) run-time, O(1) space
+		/// </summary>
+		/// <param name="numbers">numbers</param>
+		/// <returns>number with 1 occurence.</returns>
+		static int singleNumber2( const std::vector<int>& numbers )
+		{
+			auto seen = 0;
+
+			for( auto num : numbers )
+			{
+				seen ^= num;
+			}
+
+			return seen;
+		}
+	};
+
 }
--- cpp/scratch/scratch.cpp
+++ cpp/scratch/scratch.cpp
@@ -1,142 +1,81 @@
 ﻿#include <bits/stdc++.h>
 
-/*705. Design HashMap.
+/*136. Single Number.
 
-Design a HashMap without using any built-in hash table libraries.
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 
-To be specific, your design should include these functions:
+Follow up: Could you implement a solution with a linear runtime complexity and without using extra memory?
 
-put(key, value) : Insert a (key, value) pair into the HashMap. If the value already exists in the HashMap, update the value.
-get(key): Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
-remove(key) : Remove the mapping for the value key if this map contains the mapping for the key.
 
-Example:
+Example 1:
 
-MyHashMap hashMap = new MyHashMap();
-hashMap.put(1, 1);
-hashMap.put(2, 2);
-hashMap.get(1);            // returns 1
-hashMap.get(3);            // returns -1 (not found)
-hashMap.put(2, 1);          // update the existing value
-hashMap.get(2);            // returns 1
-hashMap.remove(2);          // remove the mapping for 2
-hashMap.get(2);            // returns -1 (not found)
+Input: nums = [2,2,1]
+Output: 1
+Example 2:
 
-Note:
+Input: nums = [4,1,2,1,2]
+Output: 4
+Example 3:
 
-All keys and values will be in the range of [0, 1000000].
-The number of operations will be in the range of [1, 10000].
-Please do not use the built-in HashMap library.
+Input: nums = [1]
+Output: 1
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+Each element in the array appears twice except for one element which appears only once.
 
 */
 
-class my_hash_map
+class single_number
 {
-	template<typename U, typename V>
-	struct pair
-	{
-        U first;
-        V second;
-
-		pair( U first, V second ) :
-			first{ first }, second{ second }
-		{}
-	};
-
-	class bucket
-	{
-        std::list<pair<int, int>> container_;
-
-		auto find_key( const int key )
-		{
-			return std::find_if( container_.begin(), container_.end(),
-				[key]( const pair<int, int>& current )
-				{
-					return key == current.first;
-				} );
-		}
-		
-	public:
-
-		int get( const int key )
-		{
-			const auto iter = find_key( key );
-			
-            if( iter == container_.end() ) return -1;
-
-            return iter->second;
-		}
-
-		void update( const int key, const int value )
-		{
-			const auto iter = find_key( key );
-			
-			if( iter == container_.end() )
-			{
-				container_.emplace_back( key, value );
-			}
-			else
-			{
-				iter->second = value;
-			}
-		}
-		
-		void remove( const int key )
-		{
-			const auto iter = find_key( key );
-
-			if( iter != container_.end() )
-			{
-				container_.erase( iter );
-			}
-		}
-	};
-	
-    std::vector<bucket> buckets_;
-    const int num_buckets_ = 2069;
-
-protected:
-
-    [[nodiscard]] int hash( const int key ) const
-	{
-        return key % num_buckets_;
-	}
 	
 public:
-	
-    my_hash_map()
-    {
-        buckets_ = std::vector<bucket>( num_buckets_, bucket() );
-    }
 
-    void put( const int key, const int value )
+	/// <summary>
+	/// O(N) run-time, O(1) space
+	/// </summary>
+	/// <param name="numbers">numbers</param>
+	/// <returns>the number with 1 occurence</returns>
+	static int singleNumber1( const std::vector<int>& numbers )
 	{
-		buckets_[ hash( key ) ].update( key, value );
-    }
+		auto counts = std::unordered_map<int, int>();
 
-    void remove( const int key )
+		for( auto num : numbers )
+		{
+			counts[ num ]++;
+		}
+
+		for( auto& [k, v] : counts )
+		{
+			if( v == 1 ) return k;
+		}
+
+		return -1;
+	}
+
+	/// <summary>
+	/// O(N) run-time, O(1) space
+	/// </summary>
+	/// <param name="numbers">numbers</param>
+	/// <returns>number with 1 occurence.</returns>
+	static int singleNumber2( const std::vector<int>& numbers )
 	{
-		buckets_[ hash( key ) ].remove( key );
-    }
-
-    int get( const int key )
-    {
-		return buckets_[ hash( key ) ].get( key );
-    }
+		auto seen = 0;
+		
+		for( auto num : numbers ) 
+		{
+			seen ^= num;
+		}
+		
+		return seen;
+	}
 };
 
 auto main() -> int
 {
-    auto hash_map = my_hash_map();
 
-    hash_map.put( 1, 1 );
-    hash_map.put( 2, 2 );
-    hash_map.get( 1 );            // returns 1
-    hash_map.get( 3 );            // returns -1 (not found)
-    hash_map.put( 2, 1 );          // update the existing value
-    hash_map.get( 2 );            // returns 1 
-    hash_map.remove( 2 );          // remove the mapping for 2
-    hash_map.get( 2 );            // returns -1 (not found)
-	
 	return 0;
 }
