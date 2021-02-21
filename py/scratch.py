2ba797d41a5975a7431505ebd762a1c6e504d9d1
Brandon Moretz <bmoretz82@gmail.com>
2021-02-21 16:45:29 -0500

    Python leetcode #263, logger rate limit
    Python leetcode #263, logger rate limit

--- py/scratch.py
+++ py/scratch.py
@@ -1,45 +1,68 @@
 
 '''
-219. Contains Duplicate II.
+359. Logger Rate Limiter.
 
-Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+Design a logger system that receives a stream of messages along with their timestamps. Each unique message should only be printed at most every 10 seconds
+ (i.e. a message printed at timestamp t will prevent other identical messages from being printed until timestamp t + 10).
 
+All messages will come in chronological order. Several messages may arrive at the same timestamp.
+
+Implement the Logger class:
+
+Logger() Initializes the logger object.
+bool shouldPrintMessage(int timestamp, string message) Returns true if the message should be printed in the given timestamp, otherwise returns false.
+ 
 Example 1:
 
-Input: nums = [1,2,3,1], k = 3
-Output: true
-Example 2:
+Input
+["Logger", "shouldPrintMessage", "shouldPrintMessage", "shouldPrintMessage", "shouldPrintMessage", "shouldPrintMessage", "shouldPrintMessage"]
+[[], [1, "foo"], [2, "bar"], [3, "foo"], [8, "bar"], [10, "foo"], [11, "foo"]]
+Output
+[null, true, true, false, false, false, true]
 
-Input: nums = [1,0,1,1], k = 1
-Output: true
-Example 3:
+Explanation
+Logger logger = new Logger();
+logger.shouldPrintMessage(1, "foo");  // return true, next allowed timestamp for "foo" is 1 + 10 = 11
+logger.shouldPrintMessage(2, "bar");  // return true, next allowed timestamp for "bar" is 2 + 10 = 12
+logger.shouldPrintMessage(3, "foo");  // 3 < 11, return false
+logger.shouldPrintMessage(8, "bar");  // 8 < 12, return false
+logger.shouldPrintMessage(10, "foo"); // 10 < 11, return false
+logger.shouldPrintMessage(11, "foo"); // 11 >= 11, return true, next allowed timestamp for "foo" is
+                                      // 11 + 10 = 21
+ 
+Constraints:
 
-Input: nums = [1,2,3,1,2,3], k = 2
-Output: false
+0 <= timestamp <= 109
+Every timestamp will be passed in non-decreasing order (chronological order).
+1 <= message.length <= 30
+At most 104 calls will be made to shouldPrintMessage.
 '''
 
-class contains_duplicate_ii:
+from datetime import datetime, timedelta
+class logger:
 
-    from typing import List
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.last_print = None
 
-    def containsNearbyDuplicate(self, nums: List[int], k :int ) -> bool:
-
-        result = {}
-
-        for index, item in enumerate(nums):
-            if item not in result:
-                result[item] = [index]
-            else:
-                for other in result[item]:
-                    if abs(other - index) <= k:
-                        return True
-                
-                result[item] += [index]
-
-        return False
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+        """
+        Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity.
+        """
         
+        if self.last_print is None or timedelta()
+
+
+# Your Logger object will be instantiated and called as such:
+# obj = Logger()
+# param_1 = obj.shouldPrintMessage(timestamp,message)
+
 nums, k = [1,0,1,1], 1
 
-actual = contains_duplicate_ii().containsNearbyDuplicate(nums, k)
+actual = contains_duplicate_ii.containsNearbyDuplicate(nums, k)
 
 print(actual)
